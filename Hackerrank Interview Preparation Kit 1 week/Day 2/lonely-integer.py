4776b3adba2a05b87d65cc425eff164e4b783c00
Ashwin Upadhyay <ashwinupadhyay09@gmail.com>
2024-12-25 23:44:14 +0530

    Push

---
+++ Hackerrank Interview Preparation Kit 1 week/Day 2/lonely-integer.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'lonelyinteger' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def lonelyinteger(a):
+    res = 0
+    for num in a:
+        res ^= num
+    return res 
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = lonelyinteger(a)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
